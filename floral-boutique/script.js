cddc268710c585c5cd413b1e922d29cca790576d
NataliaVozhdaeva <LomzNat@yandex.ru>
2024-02-14 19:53:22 +0400

    feat: add filter working together WIP filters clear

--- floral-boutique/script.js
+++ floral-boutique/script.js
@@ -3,44 +3,45 @@ const flowers = document.querySelector('[name="flowers"]');
 const prices = document.querySelector('[name="price"]');
 const cards = document.querySelectorAll('.card');
 
-const filterFunctionFor3 = (e, typeFilter, rg1, arg2, ard3) => {
-  cards.forEach((item) => {
-    item.style.display = 'block';
-  });
+let filters = {
+  category: [],
+  prices: [],
+};
 
-  switch (e.target.value) {
-    case rg1:
-      cards.forEach((item) => {
-        if (!item.getAttribute(typeFilter).includes(rg1)) {
-          item.style.display = 'none';
+const filterFunction = (e) => {
+  if (e.target !== category && e.target !== prices) {
+    cards.forEach((el) => (el.style.display = 'block'));
+
+    Object.values(filters).forEach((filter) => {
+      cards.forEach((el) => {
+        if (!el.getAttribute('tags').includes(filter)) {
+          el.style.display = 'none';
         }
       });
-      break;
-    case arg2:
-      cards.forEach((item) => {
-        if (!item.getAttribute(typeFilter).includes(arg2)) {
-          item.style.display = 'none';
-        }
-      });
-      break;
-    case ard3:
-      cards.forEach((item) => {
-        if (!item.getAttribute(typeFilter).includes(ard3)) {
-          item.style.display = 'none';
-        }
-      });
-      break;
-    default:
-      cards.forEach((item) => {
-        item.style.display = 'block';
-      });
+    });
   }
 };
 
 category.addEventListener('click', (e) => {
-  filterFunctionFor3(e, 'category', 'mono', 'bright', 'pastel');
+  if (e.target !== category && e.target !== prices && e.target !== flowers) {
+    filters.category = e.target.value;
+  }
+
+  filterFunction(e);
 });
 
 prices.addEventListener('click', (e) => {
-  filterFunctionFor3(e, 'price', 'less2000', 'from2to3', 'from3000');
+  if (e.target !== category && e.target !== prices && e.target !== flowers) {
+    filters.prices = e.target.value;
+  }
+
+  filterFunction(e);
+});
+
+flowers.addEventListener('click', (e) => {
+  if (e.target !== category && e.target !== prices && e.target !== flowers) {
+    filters.flowers = e.target.value;
+  }
+
+  filterFunction(e);
 });
